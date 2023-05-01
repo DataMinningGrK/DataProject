ce531e764c06dea4026acfd41ab164e3d63dfa6b
nhattan3082001 <nhattan3082001@gmail.com>
2023-05-01 08:53:03 +0700

    update clip 12-15

---
+++ src/weka/api/ClassifyInstance.java
@@ -0,0 +1,37 @@
+package weka.api;
+import weka.core.Instances;
+import weka.core.Instance;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.classifiers.bayes.NaiveBayes;
+
+public class ClassifyInstance {
+    public static void main(String args[]) throws Exception{
+        DataSource source = new DataSource("Data/Wind_data_clean.arff");
+        Instances trainDataset = source.getDataSet();
+        trainDataset.setClassIndex(trainDataset.numAttributes()-1);
+        
+
+        int numClasses = trainDataset.numClasses();
+        for (int i = 0; i< numClasses; i++){
+            String classValue = trainDataset.classAttribute().value(i);
+            System.out.println("Class Value " + i + "is" + classValue);
+        }
+        NaiveBayes nb = new NaiveBayes();
+        nb.buildClassifier(trainDataset);
+
+        DataSource source1 = new DataSource("Data/Wind_data_clean.arff");
+        Instances testDataset = source1.getDataSet();
+        testDataset.setClassIndex(testDataset.numAttributes()-1);
+
+        System.out.println("=================");
+        System.out.println("Actual Class, NB Predicted");
+        for(int i = 0; i < testDataset.numAttributes(); i++){
+            double actualClass = testDataset.instance(i).classValue();
+            String actual = testDataset.classAttribute().value((int) actualClass);
+            Instance newInst = testDataset.instance(i);
+            double predNB = nb.classifyInstance(newInst);
+            String predString = testDataset.classAttribute().value((int) predNB);
+            System.out.println(actual + ", " + predString);
+        }
+    }
+}
